c4622e75e9cac3ce860de20a0d4b62c83d9c6d5f
devpython88 <devpython88@gmail.com>
2025-04-05 12:26:54 +0530

    Delete build directory

--- build/CMakeFiles/edit_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /c/sadik/nova.h/build
-/usr/bin/ccmake.exe -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
--- build/CMakeFiles/rebuild_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /c/sadik/nova.h/build
-/usr/bin/cmake.exe --regenerate-during-build -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
